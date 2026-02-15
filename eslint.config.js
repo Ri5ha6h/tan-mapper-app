fe1ca46756374cbe4f317481b9a1d8a10764fd62
Ri5ha6h <workwithmalik97@gmail.com>
2026-02-15 15:37:02 +0530

    chore: files to ignore for lint

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,21 @@
 
 import { tanstackConfig } from "@tanstack/eslint-config"
 
-export default [...tanstackConfig]
+export default [
+    ...tanstackConfig,
+    {
+        ignores: [
+            ".output",
+            "dist",
+            "dist-ssr",
+            "node_modules",
+            ".DS_Store",
+            ".nitro",
+            ".tanstack",
+            ".wrangler",
+            ".vinxi",
+            "todos.json",
+            "/prompts",
+        ],
+    },
+]
