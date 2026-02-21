d67d24f22fa3d7a4e4cbcef9cb6965a058e714eb
Ri5ha6h <workwithmalik97@gmail.com>
2026-02-22 01:10:49 +0530

    chore: db and schema fix

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,31 @@ const config = defineConfig({
         tailwindcss(),
         tanstackStart(),
         viteReact(),
-    ],
+  ],
+  optimizeDeps: {
+      exclude: [
+          'better-auth',
+          '@better-auth/core',
+          '@better-auth/core/env',
+          '@better-auth/core/error',
+          '@better-auth/core/db',
+          '@better-auth/core/db/adapter',
+          '@better-auth/core/api',
+          '@better-auth/core/context',
+          '@better-auth/core/oauth2',
+          '@better-auth/core/social-providers',
+          '@better-auth/core/utils',
+          '@better-auth/utils',
+          '@better-auth/utils/random',
+          '@better-auth/utils/hex',
+          '@better-auth/utils/hash',
+          '@better-auth/utils/base64',
+          '@better-auth/utils/binary',
+          '@better-auth/utils/hmac',
+          '@better-auth/telemetry',
+          'postgres',
+      ],
+  },
 })
 
 export default config
